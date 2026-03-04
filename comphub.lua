0498678108a3ac83193f0e0caef786f090e5098f
robloxcomphub <132360328+robloxcomphub@users.noreply.github.com>
2026-03-04 21:47:10 +0100

    Update comphub.lua

--- comphub.lua
+++ comphub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/ff603ae844e5bc0f"))()
+loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/4894a7ec003548c9"))()
